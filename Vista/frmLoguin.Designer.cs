8d65de459368f42d48d2e3acea97c6b6eb217da7
Damián <Damin@DESKTOP-5FNUDQ7>
2025-05-14 18:03:39 -0300

    Se modificó el diseño del form login y se agregó la posibilidad de arrastrar el formulario por la pantalla

--- Vista/frmLoguin.Designer.cs
+++ Vista/frmLoguin.Designer.cs
@@ -31,14 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLoguin));
             this.btnLogin = new System.Windows.Forms.Button();
             this.pctClose = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pctBorde = new System.Windows.Forms.PictureBox();
             this.pctMinimize = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.lnkRecuperar = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pctClose)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBorde)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pctMinimize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnLogin
@@ -50,7 +52,7 @@
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogin.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.Color.Black;
-            this.btnLogin.Location = new System.Drawing.Point(326, 293);
+            this.btnLogin.Location = new System.Drawing.Point(149, 330);
             this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(152, 28);
@@ -64,7 +66,7 @@
             this.pctClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(40)))));
             this.pctClose.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pctClose.Image = global::Vista.Properties.Resources.CircleX;
-            this.pctClose.Location = new System.Drawing.Point(729, 5);
+            this.pctClose.Location = new System.Drawing.Point(389, 5);
             this.pctClose.Name = "pctClose";
             this.pctClose.Size = new System.Drawing.Size(40, 40);
             this.pctClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -72,22 +74,25 @@
             this.pctClose.TabStop = false;
             this.pctClose.Click += new System.EventHandler(this.PctClose_Click);
             // 
-            // pictureBox3
+            // pctBorde
             // 
-            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(40)))));
-            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(776, 50);
-            this.pictureBox3.TabIndex = 9;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pctBorde.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(40)))));
+            this.pctBorde.Location = new System.Drawing.Point(1, 0);
+            this.pctBorde.Name = "pctBorde";
+            this.pctBorde.Size = new System.Drawing.Size(441, 50);
+            this.pctBorde.TabIndex = 9;
+            this.pctBorde.TabStop = false;
+            this.pctBorde.Click += new System.EventHandler(this.pctBorde_Click);
+            this.pctBorde.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pctBorde_MouseDown);
+            this.pctBorde.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pctBorde_MouseMove);
+            this.pctBorde.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pctBorde_MouseUp);
             // 
             // pctMinimize
             // 
             this.pctMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(40)))));
             this.pctMinimize.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pctMinimize.Image = global::Vista.Properties.Resources.WhiteMinimize;
-            this.pctMinimize.Location = new System.Drawing.Point(6, 5);
+            this.pctMinimize.Location = new System.Drawing.Point(12, 5);
             this.pctMinimize.Name = "pctMinimize";
             this.pctMinimize.Size = new System.Drawing.Size(40, 40);
             this.pctMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -99,7 +104,7 @@
             // 
             this.textBox1.BackColor = System.Drawing.Color.White;
             this.textBox1.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(316, 146);
+            this.textBox1.Location = new System.Drawing.Point(139, 186);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(176, 23);
             this.textBox1.TabIndex = 1;
@@ -108,7 +113,7 @@
             // 
             this.txtPassword.BackColor = System.Drawing.Color.White;
             this.txtPassword.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(316, 203);
+            this.txtPassword.Location = new System.Drawing.Point(139, 243);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(176, 23);
@@ -121,39 +126,52 @@
             this.lnkRecuperar.BackColor = System.Drawing.Color.Transparent;
             this.lnkRecuperar.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lnkRecuperar.LinkColor = System.Drawing.Color.White;
-            this.lnkRecuperar.Location = new System.Drawing.Point(315, 252);
+            this.lnkRecuperar.Location = new System.Drawing.Point(138, 292);
             this.lnkRecuperar.Name = "lnkRecuperar";
             this.lnkRecuperar.Size = new System.Drawing.Size(177, 16);
             this.lnkRecuperar.TabIndex = 3;
+            this.lnkRecuperar.TabStop = true;
             this.lnkRecuperar.Text = "Olvidé mi usuario/contraseña";
             this.lnkRecuperar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkRecuperar_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(157, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
             // 
             // FrmLoguin
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(773, 464);
+            this.ClientSize = new System.Drawing.Size(441, 376);
             this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lnkRecuperar);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pctMinimize);
             this.Controls.Add(this.pctClose);
             this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pctBorde);
             this.ForeColor = System.Drawing.SystemColors.Control;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "FrmLoguin";
-            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menú";
             this.Load += new System.EventHandler(this.FrmLoguin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pctClose)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBorde)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pctMinimize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,11 +180,12 @@
         #endregion
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.PictureBox pctClose;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pctBorde;
         private System.Windows.Forms.PictureBox pctMinimize;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.LinkLabel lnkRecuperar;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Vista/frmLoguin.cs
+++ Vista/frmLoguin.cs
@@ -56,5 +56,49 @@ namespace Vista
         {
 
         }
+
+        private void pctBorde_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int mouse, mousex, mousey;
+
+        private void pctBorde_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse = 1;
+            mousex = e.X;
+            mousey = e.Y;
+        }
+
+        private void pctBorde_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse = 0;
+        }
+
+        private void pctBorde_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouse == 1)
+            {
+                int newX = MousePosition.X - mousex;
+                int newY = MousePosition.Y - mousey;
+
+                // Obtener el área de trabajo de la pantalla (sin la barra de tareas)
+                Rectangle screenBounds = Screen.FromControl(this).WorkingArea;
+
+                // Limitar la nueva posición del formulario dentro de la pantalla
+                if (newX < screenBounds.Left)
+                    newX = screenBounds.Left;
+                if (newY < screenBounds.Top)
+                    newY = screenBounds.Top;
+                if (newX + this.Width > screenBounds.Right)
+                    newX = screenBounds.Right - this.Width;
+                if (newY + this.Height > screenBounds.Bottom)
+                    newY = screenBounds.Bottom - this.Height;
+
+                this.SetDesktopLocation(newX, newY);
+            }
+        }
     }
 }
+
