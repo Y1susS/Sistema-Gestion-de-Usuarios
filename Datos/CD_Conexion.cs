26079407703dc68f4b59728dc2d2ea38256f128d
alanwach0 <prietoalanm@gmail.com>
2025-05-26 20:44:30 -0300

    Se crea clase conexion y clase usuario

---
+++ Datos/CD_Conexion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class CD_Conexion
+    {
+        private string cadena = "Data Source=.;Initial Catalog = SistemaGestionUsuario; Integrated Security = True;";
+        private SqlConnection oConn;
+
+        public CD_Conexion()
+        {
+            oConn = new SqlConnection(cadena);
+        }
+        public SqlConnection AbrirConexion()
+        {
+            if (oConn.State == ConnectionState.Open)
+            {
+                oConn.Close();
+            }
+            else
+            {
+                oConn.Open();
+            }
+            return oConn;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (oConn.State == ConnectionState.Open)
+            {
+                oConn.Close();
+            }
+            else
+            {
+                oConn.Close();
+            }
+            return oConn;
+        }
+
+
+    }
+}
---
+++ Datos/CD_Usuarios.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class CD_Usuarios
+    {
+        public CD_Conexion conexion = new CD_Conexion();
+        SqlCommand comando = new SqlCommand();
+        public bool AgregarUsuario(string id, string usuario, string email, string password, string rol)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "altaUsuario";
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@Id:User", id);
+            comando.Parameters.AddWithValue("@Usuario", usuario);
+            comando.Parameters.AddWithValue("@Email", email);
+            comando.Parameters.AddWithValue("@Password", password);
+            comando.Parameters.AddWithValue("@Rol", rol);
+
+            int filasAfectadas = comando.ExecuteNonQuery();
+            conexion.CerrarConexion();
+
+            return filasAfectadas > 0;
+        }
+
+        public bool ModificarUsuario(int id, string usuario, string email, string password, bool activo, int rol)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "modificarUsuario";
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@Id_user", id);
+            comando.Parameters.AddWithValue("@User", usuario);
+            comando.Parameters.AddWithValue("@Password", password);
+            comando.Parameters.AddWithValue("@Email", email);
+            comando.Parameters.AddWithValue("@Activo", activo);
+            comando.Parameters.AddWithValue("@Id_Rol", rol);
+
+            int filasAfectadas = comando.ExecuteNonQuery();
+            conexion.CerrarConexion();
+
+            return filasAfectadas > 0;
+        }
+
+        public bool ValidarUsuario(string usuario, string password)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "validarUsuario";
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@User", usuario);
+            comando.Parameters.AddWithValue("@Password", password);
+
+            object resultado = (int)comando.ExecuteScalar();
+            conexion.CerrarConexion();
+
+            if (resultado != null && int.TryParse(resultado.ToString(), out int valor))
+            {
+                return valor == 1;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+    }
+}
