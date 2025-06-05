81934f2c48bc510f93fd74b8b807f1a0dbf432e5
Lucasgianturco <Lucasgianturco@hotmail.com>
2025-06-04 21:48:55 -0300

    se agregaron y editaron formularios

--- Vista/frmPermisos.cs
+++ Vista/frmPermisos.cs
@@ -16,5 +16,18 @@ namespace Vista
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            cmbRoles.Items.Add("Vendedor");
+            cmbRoles.Items.Add("Administrador");
+            cmbRoles.SelectedIndex = 1; //por defecto el primer rol para asignar siempre es vendedor
+
+            //si tenemos una lista de roles en base de datos, se pueden traer esos roles con la funcion
+            
+            //List<string> roles = new List<string> { "Administrador", "Usuario General", "Supervisor" };
+            //comboBoxRoles.Items.AddRange(roles.ToArray());
+        }
     }
 }
